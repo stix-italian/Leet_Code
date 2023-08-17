232b6c86a1e81a2106087f568d93aad9f17a66fc
stix-italian <stix.italian@gmail.com>
2023-08-17 06:00:37 -0600

    for loop looping after element is deleted.

--- asteroids.cpp
+++ asteroids.cpp
@@ -4,38 +4,44 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         for (int i=0; i < asteroids.size()-1; i++){
-            for (int j=1; j < asteroids.size(); j++){
-                if (asteroids[i] > 0 && asteroids[j] > 0){
-                    std::cout << "i > j: " << asteroids[i] << " " << asteroids[j] << endl;
-                    ;
+            cout << "i is: " << i << endl;
+            for (auto a: asteroids){
+                        std::cout << a << " ";
+                    }
+                    cout << "end of starting vector." << endl;
+            if (asteroids[i] > 0 && asteroids[i+1] > 0){
+                std::cout << "both are positive no collision " << asteroids[i] << " " << asteroids[i+1] << endl;
+                ;
+            }
+            else if (asteroids[i] < 0 && asteroids[i+1] < 0){
+                std::cout << "both are negative no collision " << asteroids[i] << " " << asteroids[i+1] << endl;
+                ;
+            }
+            else if (asteroids[i+1] < 0){
+                if (asteroids[i] < abs(asteroids[i+1])){
+                    std::cout << "i < i+1 delete i" << endl;
+                    asteroids.erase(asteroids.begin()+i);
+                    for (auto a: asteroids){
+                        std::cout << a << " ";
+                    }
+                    cout << endl;
                 }
-                else if (asteroids[i] < 0 && asteroids[j] < 0){
-                    std::cout << "i < j: " << asteroids[i] << " " << asteroids[j] << endl;
-                    ;
+                else if (asteroids[i] > abs(asteroids[i+1])){
+                    asteroids.erase(asteroids.begin()+i+1);
+                    std::cout << "i > i+1 delete i+1" << endl;
+                    for (auto a: asteroids){
+                        std::cout << a << " ";
+                    }
+                    cout << endl;
                 }
-                else if (asteroids[j] < 0){
-                    if (asteroids[i] < abs(asteroids[j])){
-                        std::cout << "i < j delete i";
-                        asteroids.erase(asteroids.begin()+i);
-                        for (auto a: asteroids){
-                            std::cout << a << " ";
-                        }
+                else {
+                    asteroids.erase(asteroids.begin()+i+1);
+                    asteroids.erase(asteroids.begin()+i);
+                    std::cout << "i == i+1 delete both" << endl;
+                    for (auto a: asteroids){
+                        std::cout << a << " ";
                     }
-                    else if (asteroids[i] > abs(asteroids[j])){
-                        asteroids.erase(asteroids.begin()+j);
-                        std::cout << "i > j delete j";
-                        for (auto a: asteroids){
-                            std::cout << a << " ";
-                        }
-                    }
-                    else {
-                        asteroids.erase(asteroids.begin()+j);
-                        asteroids.erase(asteroids.begin()+i);
-                        std::cout << "i == j delete both";
-                        for (auto a: asteroids){
-                            std::cout << a << " ";
-                        }
-                    }
+                    cout << endl;
                 }
             }
         }
